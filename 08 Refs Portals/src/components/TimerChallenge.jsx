8faf832bef7bbdf33e2f96a2438b2a3ad43fc330
Musab <malqarninet@gmail.com>
2024-02-05 23:15:04 +0300

    forwarding a ref, used when we pass a ref to another component, setTimeInterval to execute timer

--- 08 Refs Portals/src/components/TimerChallenge.jsx
+++ 08 Refs Portals/src/components/TimerChallenge.jsx
@@ -1,47 +1,51 @@
-import { useState } from "react";
+import { useState, useRef } from 'react';
 
-// if we add variable outside component will not recrated
-// below variable will be shared across all components instances 
-// let timer; not a good solution
+import ResultModal from './ResultModal.jsx';
 
-const TimerChallenge = ({ title, targetTime }) => {
-    const [timeerStarted, setTimeerStarted] = useState(false);
-    const [timeExpired, setTimeExpired] = useState(false);
+// let timer;
+
+export default function TimerChallenge({ title, targetTime }) {
     const timer = useRef();
+    const dialog = useRef();
 
-    // will not chnage as state
-    // let timer; not a good soltion since when clich handleStop 
-    // not reconised timer and component not refreshing
+    const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
 
-    const handleStart = () => {
-        timer.current = setTimeout(() => {
-            setTimeExpired(true);
-        }, targetTime * 1000)
-        setTimeerStarted(true);
+    const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+    if(timeRemaining <= 0){
+        clearInterval(timer.current);
+        setTimeRemaining(targetTime * 1000);
+        dialog.current.open();
     }
 
-    const handleStop = () => {
-        clearTimeout(timer.current);
+    function handleStart() {
+        timer.current = setInterval(() => {
+            setTimeRemaining(prevTime => prevTime - 10);
+        }, 10);
+    }
+
+    function handleStop() {
+        dialog.current.open();
+        clearInterval(timer.current);
     }
 
     return (
-        <section className="challenge">
-            <h2>{title}</h2>
-            {timeExpired && <p>You lost!</p>}
-            <p className="challenge-time">
-                {targetTime} second {targetTime > 1 ? "s" : ""}
-            </p>
-            <p>
-                <button onClick={timeerStarted ? handleStop : handleStart}>
-                    {timeerStarted ? "Stop" : "Start"} Challenge
-                </button>
-
-            </p>
-            <p className="">
-                {timeerStarted ? "Time is running..." : "Timer inactive"}
-            </p>
-        </section>
+        <>
+            <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+            <section className="challenge">
+                <h2>{title}</h2>
+                <p className="challenge-time">
+                    {targetTime} second{targetTime > 1 ? 's' : ''}
+                </p>
+                <p>
+                    <button onClick={timerIsActive ? handleStop : handleStart}>
+                        {timerIsActive ? 'Stop' : 'Start'} Challenge
+                    </button>
+                </p>
+                <p className={timerIsActive ? 'active' : undefined}>
+                    {timerIsActive ? 'Time is running...' : 'Timer inactive'}
+                </p>
+            </section>
+        </>
     );
 }
-
-export default TimerChallenge;
