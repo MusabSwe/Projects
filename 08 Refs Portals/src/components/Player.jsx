443f19d91235d97eda9b8cd24853d80bbb0effdd
MUSAB ALQARNI <78758932+MusabSwe@users.noreply.github.com>
2024-07-29 21:52:59 +0300

    ref

--- 08 Refs Portals/src/components/Player.jsx
+++ 08 Refs Portals/src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 export default function Player() {
+  const [playerName, setPlayerName] = useState('');
+  const [isShowName, setIsShowName] = useState(false);
+
+  const handleClick = () => {
+    setIsShowName(true);
+  }
+
+  const handleNameChange = (e) => {
+    setIsShowName(false);
+    setPlayerName(e?.target?.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {isShowName ? playerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleNameChange} value={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
