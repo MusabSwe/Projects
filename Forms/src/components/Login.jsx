24b84cffbaaaa3552795fd16cff233fdc46a953a
Musab <malqarninet@gmail.com>
2025-01-26 14:41:39 +0300

    handdling form submittion

--- Forms/src/components/Login.jsx
+++ Forms/src/components/Login.jsx
@@ -1,6 +1,10 @@
 export default function Login() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('handleSubmit');
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
